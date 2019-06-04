3ec7870c4cadcd19c5c0c86b5a470e1c211b2ece
Andrés Fernández <andres.fernandez@insinnospain.com>
2019-06-04 14:56:46 +0200

    Corregido Error

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -8,40 +7,12 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-	
-  constructor(private camera: Camera, public photoService: PhotoService) {}
+  currentImage: any;
 
-takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-this.camera.getPicture(options).then((imageData) => {
-  // Add new photo to gallery
-  this.photos.unshift({
-    data: 'data:image/jpeg;base64,' + imageData
-  });
+  constructor(public photoService: PhotoService) {  }
 
-  // Save all photos for later viewing
-  this.storage.set('photos', this.photos);
-}, (err) => {
-  // Handle error
-  console.log("Camera issue: " + err);
-});
-  
-loadSaved() {
-  this.storage.get('photos').then((photos) => {
-    this.photos = photos || [];
-  });
-}
-
-ngOnInit() {
-  this.photoService.loadSaved();
-}
- 
+  ngOnInit() {
+    this.photoService.loadSaved();
   }
-
 
 }
